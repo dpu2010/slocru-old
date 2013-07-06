8fb65698b16f67500d5076058df0460fdfdaeab0
Stephen Hill <dpu2010@gmail.com>
2013-07-06 13:27:46 -0700

    7/6/2013 - SRH Separated header view code from other views

---
+++ htdocs/application/views/header/header.php
@@ -0,0 +1,16 @@
+
+<html>
+    <head>
+        <title>Slo Cru | Campus Crusade for Christ at Cal Poly and Cuesta</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        <div class="navbar navbar-inverse">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="brand" href = '#'><strong>Welcome</strong></a>
+                </div>
+            </div>
+        </div>
--- htdocs/application/views/home.php
+++ htdocs/application/views/home.php
@@ -1,18 +1,3 @@
-<html>
-    <head>
-        <title>Slo Cru | Campus Crusade for Christ at Cal Poly and Cuesta</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    </head>
-    <body>
-        <div class="navbar navbar-inverse">
-            <div class="navbar-inner">
-                <div class="container">
-                    <a class="brand" href = '#'><strong>Welcome</strong></a>
-                </div>
-            </div>
-        </div>
         <div class="row">
             <div class="span12 offset1">
                 Hello wonderful people!<br/><br/>
