006873fdef5dd9a7196e2ff1892236f8cf00c404
Stephen Hill <dpu2010@gmail.com>
2013-07-06 03:42:12 -0700

    7/6/2013 - SRH Installed twitter bootstrap and created welcome message for the team

--- htdocs/application/views/home.php
+++ htdocs/application/views/home.php
@@ -1,4 +1,30 @@
-<?php
-
-echo "hi";
-?>
+<html>
+    <head>
+        <title>Slo Cru | Campus Crusade for Christ at Cal Poly and Cuesta</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        <div class="navbar navbar-inverse">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="brand" href = '#'><strong>Welcome</strong></a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="span12 offset1">
+                Hello wonderful people!<br/><br/>
+                First of all, I want to thank you guys so much for your help this summer. This website CANNOT be completed without your guys' help!<br/>
+                Second, this will be our development site for now. We will upload all our work here so we can all see it!<br/>
+                I look forward to working with each and every one of you!
+                Stephen<br/><br/>
+                And the peace of God, which surpasses all understanding, will guard your hearts and your minds in Christ Jesus<br/>
+                Philippians 4:7
+            </div>
+        </div>
+        <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    </body>
+</html>
