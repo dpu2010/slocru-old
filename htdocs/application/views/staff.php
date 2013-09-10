48d5cfacc96ef8ada020055fb1d6c82f02d7773a
Stephen Hill <dpu2010@gmail.com>
2013-09-09 21:15:14 -0700

    9/9/2013 - SRH Staff page layout

--- htdocs/application/views/staff.php
+++ htdocs/application/views/staff.php
@@ -1,5 +1,84 @@
-<br />
-<br />
-<br />
-<br />
-<button>Darren</button>
+<div class="container">
+    <br/>
+    <div class="col-md-12" style="text-align: center; font-size: 36pt;">
+        Staff
+    </div>
+    <br/>
+    <img src="../assets/img/dropshadowup2.png" style="width:100%; height:25px;"/>
+    <div class="row">
+        <div class="col-md-4" style="text-align: center;">
+            <img src="http://placehold.it/140x140" class="img-circle" style="padding: 4px; border: 1px solid #ccc; background-color: white;" />
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            <img src="http://placehold.it/140x140" class="img-circle" style="padding: 4px; border: 1px solid #ccc; background-color: white;" />
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            <img src="http://placehold.it/140x140" class="img-circle" style="padding: 4px; border: 1px solid #ccc; background-color: white;" />
+        </div>
+    </div>
+    <br/>
+    <div class="row">
+        <div class="col-md-4" style="text-align: center;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget lacinia tellus. Nulla at eleifend orci. Suspendisse nec nisi diam. Ut lorem diam, vehicula quis erat quis, faucibus vulputate nulla. Quisque ac convallis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante quam, molestie nec sapien quis, mollis condimentum mi. Nullam at luctus tellus. Donec sagittis ipsum mi, in mattis leo tempus sit amet. Proin varius augue ante, a consectetur turpis consectetur et. Fusce venenatis nisi lacus, at semper magna porttitor non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet orci nisl, ut egestas dolor sollicitudin ut.
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget lacinia tellus. Nulla at eleifend orci. Suspendisse nec nisi diam. Ut lorem diam, vehicula quis erat quis, faucibus vulputate nulla. Quisque ac convallis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante quam, molestie nec sapien quis, mollis condimentum mi. Nullam at luctus tellus. Donec sagittis ipsum mi, in mattis leo tempus sit amet. Proin varius augue ante, a consectetur turpis consectetur et. Fusce venenatis nisi lacus, at semper magna porttitor non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet orci nisl, ut egestas dolor sollicitudin ut.
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget lacinia tellus. Nulla at eleifend orci. Suspendisse nec nisi diam. Ut lorem diam, vehicula quis erat quis, faucibus vulputate nulla. Quisque ac convallis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante quam, molestie nec sapien quis, mollis condimentum mi. Nullam at luctus tellus. Donec sagittis ipsum mi, in mattis leo tempus sit amet. Proin varius augue ante, a consectetur turpis consectetur et. Fusce venenatis nisi lacus, at semper magna porttitor non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet orci nisl, ut egestas dolor sollicitudin ut.
+        </div>
+    </div>
+    <br/>
+    <div class="row">
+        <div class="col-md-4" style="text-align: center;">
+            <img src="http://placehold.it/140x140" class="img-circle" style="padding: 4px; border: 1px solid #ccc; background-color: white;" />
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            <img src="http://placehold.it/140x140" class="img-circle" style="padding: 4px; border: 1px solid #ccc; background-color: white;" />
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            <img src="http://placehold.it/140x140" class="img-circle" style="padding: 4px; border: 1px solid #ccc; background-color: white;" />
+        </div>
+    </div>
+    <br/>
+    <div class="row">
+        <div class="col-md-4" style="text-align: center;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget lacinia tellus. Nulla at eleifend orci. Suspendisse nec nisi diam. Ut lorem diam, vehicula quis erat quis, faucibus vulputate nulla. Quisque ac convallis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante quam, molestie nec sapien quis, mollis condimentum mi. Nullam at luctus tellus. Donec sagittis ipsum mi, in mattis leo tempus sit amet. Proin varius augue ante, a consectetur turpis consectetur et. Fusce venenatis nisi lacus, at semper magna porttitor non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet orci nisl, ut egestas dolor sollicitudin ut.
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget lacinia tellus. Nulla at eleifend orci. Suspendisse nec nisi diam. Ut lorem diam, vehicula quis erat quis, faucibus vulputate nulla. Quisque ac convallis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante quam, molestie nec sapien quis, mollis condimentum mi. Nullam at luctus tellus. Donec sagittis ipsum mi, in mattis leo tempus sit amet. Proin varius augue ante, a consectetur turpis consectetur et. Fusce venenatis nisi lacus, at semper magna porttitor non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet orci nisl, ut egestas dolor sollicitudin ut.
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget lacinia tellus. Nulla at eleifend orci. Suspendisse nec nisi diam. Ut lorem diam, vehicula quis erat quis, faucibus vulputate nulla. Quisque ac convallis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante quam, molestie nec sapien quis, mollis condimentum mi. Nullam at luctus tellus. Donec sagittis ipsum mi, in mattis leo tempus sit amet. Proin varius augue ante, a consectetur turpis consectetur et. Fusce venenatis nisi lacus, at semper magna porttitor non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet orci nisl, ut egestas dolor sollicitudin ut.
+        </div>
+    </div>
+    <br/>
+    <div class="col-md-12" style="text-align: center; font-size: 36pt;">
+        Interns
+    </div>
+    <br/>
+    <img src="../assets/img/dropshadowup2.png" style="width:100%; height:25px;"/>
+    <div class="row">
+        <div class="col-md-4" style="text-align: center;">
+            <img src="http://placehold.it/140x140" class="img-circle" style="padding: 4px; border: 1px solid #ccc; background-color: white;" />
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            <img src="http://placehold.it/140x140" class="img-circle" style="padding: 4px; border: 1px solid #ccc; background-color: white;" />
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            <img src="http://placehold.it/140x140" class="img-circle" style="padding: 4px; border: 1px solid #ccc; background-color: white;" />
+        </div>
+    </div>
+    <br/>
+    <div class="row">
+        <div class="col-md-4" style="text-align: center;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget lacinia tellus. Nulla at eleifend orci. Suspendisse nec nisi diam. Ut lorem diam, vehicula quis erat quis, faucibus vulputate nulla. Quisque ac convallis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante quam, molestie nec sapien quis, mollis condimentum mi. Nullam at luctus tellus. Donec sagittis ipsum mi, in mattis leo tempus sit amet. Proin varius augue ante, a consectetur turpis consectetur et. Fusce venenatis nisi lacus, at semper magna porttitor non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet orci nisl, ut egestas dolor sollicitudin ut.
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget lacinia tellus. Nulla at eleifend orci. Suspendisse nec nisi diam. Ut lorem diam, vehicula quis erat quis, faucibus vulputate nulla. Quisque ac convallis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante quam, molestie nec sapien quis, mollis condimentum mi. Nullam at luctus tellus. Donec sagittis ipsum mi, in mattis leo tempus sit amet. Proin varius augue ante, a consectetur turpis consectetur et. Fusce venenatis nisi lacus, at semper magna porttitor non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet orci nisl, ut egestas dolor sollicitudin ut.
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget lacinia tellus. Nulla at eleifend orci. Suspendisse nec nisi diam. Ut lorem diam, vehicula quis erat quis, faucibus vulputate nulla. Quisque ac convallis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante quam, molestie nec sapien quis, mollis condimentum mi. Nullam at luctus tellus. Donec sagittis ipsum mi, in mattis leo tempus sit amet. Proin varius augue ante, a consectetur turpis consectetur et. Fusce venenatis nisi lacus, at semper magna porttitor non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet orci nisl, ut egestas dolor sollicitudin ut.
+        </div>
+    </div>
+</div>
