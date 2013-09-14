b38ee970b7f85393a2d11122b3d1f57d117f1e2e
Stephen Hill <dpu2010@gmail.com>
2013-09-13 19:04:32 -0700

    9/13/2013 - SRH The start of the site map

--- htdocs/application/views/footer/footer.php
+++ htdocs/application/views/footer/footer.php
@@ -1,2 +1,13 @@
+    <div class="container" style="padding: 10px 100px 10px 100px;">
+        <div class="col-md-4" style="text-align: center;">
+            Site map
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            Site map
+        </div>
+        <div class="col-md-4" style="text-align: center;">
+            Site map
+        </div>
+    </div>
 </body>
 </html>
