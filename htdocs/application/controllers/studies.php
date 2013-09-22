93abbc949cfe13e9dd67612397aaa91ae6cb4430
Stephen Hill <dpu2010@gmail.com>
2013-09-21 17:31:53 -0700

    9/20/2013 - SRH Update study leaders emails

--- htdocs/application/controllers/studies.php
+++ htdocs/application/controllers/studies.php
@@ -26,7 +26,7 @@ class Studies extends CI_Controller {
         $interestArr = array(
             'Asian Americans' => 'tyler.mendoza@cru.org',
             'Athletes' => 'matt.williams@cru.org',
-            'Greeks' => 'melanie.hanlon@cru.org',
+            'Greeks' => array('Female' => 'melanie.hanlon@cru.org', 'Male' => 'reid.zeller@cru.org'),
             'Latino/Hispanic' => 'reid.zeller@cru.org',
             'Transfers' => 'kathryn.edmiston@cru.org'
         );
@@ -34,40 +34,40 @@ class Studies extends CI_Controller {
             'Male' => array(
                 'Cal Poly SLO' => array(
                     'Freshmen' => array(
-                        'Yosemite' => array('kyle.fletcher@cru.org'),
-                        'Sierra Madre' => array('kyle.fletcher@cru.org'),
-                        'Tenaya' => array('kyle.fletcher@cru.org'),
-                        'Trinity' => array('kyle.fletcher@cru.org'),
-                        'Santa Lucia' => array('kyle.fletcher@cru.org'),
-                        'North Mountain' => array('kyle.fletcher@cru.org'),
-                        'Muir' => array('kyle.fletcher@cru.org'),
-                        'Fremont' => array('kyle.fletcher@cru.org'),
-                        'Sequoia' => array('kyle.fletcher@cru.org'),
-                        'Cerro Vista' => array('kyle.fletcher@cru.org'),
-                        'PCV' => array('kyle.fletcher@cru.org'),
-                        'Other' => array('kyle.fletcher@cru.org')
+                        'Yosemite' => array('dwagstaf@calpoly.edu', 'lundquis@calpoly.edu', 'rcarey@calpoly.edu', 'rtpate@calpoly.edu'),
+                        'Sierra Madre' => array('ebodenbach@gmail.com', 'celind@calpoly.edu', 'mhamada@calpoly.edu', 'alek.thelinquist@gmail.com'),
+                        'Tenaya' => array('jdavis45@calpoly.edu', 'ljschroe@calpoly.edu'),
+                        'Trinity' => array('bgreller@calpoly.edu', 'cmhennin@calpoly.edu'),
+                        'Santa Lucia' => array('thoekstr@calpoly.edu', 'john.simeroth@gmail.com'),
+                        'North Mountain' => array('darren12_11@yahoo.com.tw', 'jtbarkhimer@gmail.com'),
+                        'Muir' => array('samuelxlui@gmail.com', 'awsnyder21@gmail.com'),
+                        'Fremont' => array('craigcary@ymail.com', 'angelsfan1@gmail.com'),
+                        'Sequoia' => array('kportis@calpoly.edu', 'tsevern@calpoly.edu'),
+                        'Cerro Vista' => array('aalvar28@calpoly.edu'),
+                        'PCV' => array('jonathan.newman@cru.org'),
+                        'Other' => array('jonathan.newman@cru.org')
                     ),
-                    'Sophomore' => 'kyle.fletcher@cru.org',
-                    'Junior' => 'kyle.fletcher@cru.org',
-                    'Senior' => 'kyle.fletcher@cru.org',
+                    'Sophomore' => 'jonathan.newman@cru.org',
+                    'Junior' => 'jonathan.newman@cru.org',
+                    'Senior' => 'jonathan.newman@cru.org',
                     'Senior plus' => 'kyle.fletcher@cru.org'
                 ),
-                'Cuesta' => 'adam.nixon@cru.org'
+                'Cuesta' => 'jonathan.newman@cru.org'
             ),
             'Female' => array(
                 'Cal Poly SLO' => array(
                     'Freshmen' => array(
-                        'Yosemite' => array('sjmay@calpoly.edu','ericaandris@yahoo.com','klbriggs@calpoly.edu','kwalke10@calpoly.edu'),
-                        'Sierra Madre' => array('scdean92@gmail.com','ceshanno@calpoly.edu','h.kirtland@comcast.net','regan.kate8@gmail.com'),
-                        'Tenaya' => array('paigekeating@yahoo.com','prystrom@gmail.com'),
-                        'Trinity' => array('aarimura@calpoly.edu'),
-                        'Santa Lucia' => array('pporche@calpoly.edu'),
-                        'North Mountain' => array('bri_sny@sbcglobal.net'),
-                        'Muir' => array('yourleftshoe@comcast.net'),
-                        'Fremont' => array('mnbarnet@calpoly.edu','kbriggs26@yahoo.com'),
-                        'Sequoia' => array('krodabau@calpoly.edu','ccollom@calpoly.edu'),
-                        'Cerro Vista' => array('kbjrkman@calpoly.edu','kathleengrigsby@yahoo.com','kierstendemmond@yahoo.com','tawalker@calpoly.edu'),
-                        'PCV' => array('bkamp33@gmail.com','shwhite@calpoly.edu'),
+                        'Yosemite' => array('sjmay@calpoly.edu', 'ericaandris@yahoo.com', 'klbriggs@calpoly.edu', 'kwalke10@calpoly.edu'),
+                        'Sierra Madre' => array('scdean92@gmail.com', 'ceshanno@calpoly.edu', 'h.kirtland@comcast.net', 'regan.kate8@gmail.com'),
+                        'Tenaya' => array('paigekeating@yahoo.com', 'prystrom@gmail.com'),
+                        'Trinity' => array('aarimura@calpoly.edu', 'pvase@calpoly.edu', 'rebekkahtuck@aol.com', 'mmstewar@calpoly.edu'),
+                        'Santa Lucia' => array('pporche@calpoly.edu', 'bri_sny@sbcglobal.net'),
+                        'North Mountain' => array('pporche@calpoly.edu', 'bri_sny@sbcglobal.net'),
+                        'Muir' => array('yourleftshoe@comcast.net', 'cjwalter@calpoly.edu'),
+                        'Fremont' => array('mnbarnet@calpoly.edu', 'kbriggs26@yahoo.com'),
+                        'Sequoia' => array('krodabau@calpoly.edu', 'ccollom@calpoly.edu'),
+                        'Cerro Vista' => array('kbjrkman@calpoly.edu', 'kathleengrigsby@yahoo.com', 'kierstendemmond@yahoo.com', 'tawalker@calpoly.edu'),
+                        'PCV' => array('bkamp33@gmail.com', 'shwhite@calpoly.edu'),
                         'Other' => array('gretchen.pappas@cru.org')
                     ),
                     'Sophomore' => 'gretchen.pappas@cru.org',
@@ -91,53 +91,55 @@ class Studies extends CI_Controller {
         {
             foreach ($interests as $value)
             {
-                if(($value === 'Greeks' && $gender === 'Male')||($value === 'Transfers' && $gender === 'Male')){}
+                if ($value === 'Greeks')
+                    array_push($use_email, $interestArr[$value][$gender]);
                 else
-                    array_push($use_email, $interestArr[$value]);
+                {
+                    if ($value === 'Transfers' && $gender === 'Male') {}
+                    else
+                        array_push($use_email, $interestArr[$value]);
+                }
             }
         }
-         $config = Array(
-          'protocol' => 'smtp',
-          'smtp_host' => 'mail.slocrusade.com',
-          'smtp_port' => 26,
-          'smtp_user' => 'admin+slocrusade.com',
-          'smtp_pass' => 'romans125',
-          'mailtype' => 'html',
-          'charset' => 'iso-8859-1'
-          );
-          $this->load->library('email', $config);
-          $this->email->set_newline("\r\n");
+        $config = Array(
+            'protocol' => 'smtp',
+            'smtp_host' => 'mail.slocrusade.com',
+            'smtp_port' => 26,
+            'smtp_user' => 'admin+slocrusade.com',
+            'smtp_pass' => 'romans125',
+            'mailtype' => 'html',
+            'charset' => 'iso-8859-1'
+        );
+        $this->load->library('email', $config);
+        $this->email->set_newline("\r\n");
 
-          $this->email->from('admin@slocrusade.com');
-          $this->email->to($use_email);
-          $this->email->bcc('dpu2010@gmail.com');
+        $this->email->from('admin@slocrusade.com');
+        $this->email->to($use_email);
+        $this->email->bcc('dpu2010@gmail.com');
 
-          $this->email->subject('Student interested in a Bible Study!');
-          $str = 'This student is interested in a bible study. Please contact them as soon as possible. Thank you!<br/><br/>';
-          $str .= 'Name: '.$name.'<br/>';
-          $str .= 'Gender: '.$gender.'<br/>';
-          $str .= 'College: '.$college.'<br/>';
-          $str .= 'Year in school: '.$year.'<br/>';
-          if($year === 'Freshmen' && $college !== 'Cuesta')
-          {
-            $str .= 'Dorm: '.$dorm.'<br/>';
-            $str .= 'Room: '.$room.'<br/>';
-          }
-          $str .= 'Email: '.$email.'<br/>';
-          $str .= 'Cell: '.$cell.'<br/>';
-          if(!empty($interests))
-          {
-              $str .= 'Interests: <br/>';
-              foreach($interests as $value)
-              {
-                  $str .= '&nbsp;&nbsp;&nbsp;&nbsp;'.$value.'<br/>';
-              }
-          }
-          $str .= 'Notes: '.$notes.'<br/>';
-          $str .= '<br/>--Admin';
-          $this->email->message($str);
-          $this->email->send();
-          redirect('/studies', 'refresh'); 
+        $this->email->subject('Student interested in a Bible Study!');
+        $str = 'This student is interested in a bible study. Please contact them as soon as possible. Thank you!<br/><br/>';
+        $str .= 'Name: ' . $name . '<br/>';
+        $str .= 'Gender: ' . $gender . '<br/>';
+        $str .= 'College: ' . $college . '<br/>';
+        $str .= 'Year in school: ' . $year . '<br/>';
+        if ($year === 'Freshmen' && $college !== 'Cuesta') {
+            $str .= 'Dorm: ' . $dorm . '<br/>';
+            $str .= 'Room: ' . $room . '<br/>';
+        }
+        $str .= 'Email: ' . $email . '<br/>';
+        $str .= 'Cell: ' . $cell . '<br/>';
+        if (!empty($interests)) {
+            $str .= 'Interests: <br/>';
+            foreach ($interests as $value) {
+                $str .= '&nbsp;&nbsp;&nbsp;&nbsp;' . $value . '<br/>';
+            }
+        }
+        $str .= 'Notes: ' . $notes . '<br/>';
+        $str .= '<br/>--Admin';
+        $this->email->message($str);
+        $this->email->send();
+        redirect('/studies', 'refresh');
     }
 
 }
