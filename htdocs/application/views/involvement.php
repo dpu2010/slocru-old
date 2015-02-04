033ee7d31eae7a7899b671972e8ed3d40375c8b4
Stephen <Stephen@192.168.2.17>
2015-02-03 21:50:10 -0800

    Made involvement page

--- htdocs/application/views/involvement.php
+++ htdocs/application/views/involvement.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <div class="container">
     <div>
         <div class="left col-md-8">
@@ -7,41 +8,108 @@
             </div>
             <hr/>
             <div class="box">
-                <div class="white-box">
-                    <img src="../assets/img/involvement/community.jpg"/>
-                    <br/>
-                    COMMUNITY GROUPS
-                </div>
+                <a href="<?php echo base_url() . "studies" ?>">
+                    <div class="white-box">
+                        <img src="../assets/img/involvement/community.jpg"/>
+                        <br/>
+                        COMMUNITY GROUPS
+                    </div>
+                </a>
             </div>
             <div class="box">
-                <div class="white-box">
-                    <img src="../assets/img/involvement/ministry.jpg"/>
-                    <br/>
-                    MINISTRY TEAMS
-                </div>
+                <a href="<?php echo base_url() . "ministry" ?>">
+                    <div class="white-box">
+                        <img src="../assets/img/involvement/ministry.jpg"/>
+                        <br/>
+                        MINISTRY TEAMS
+                    </div>
+                </a>
             </div>
 
             <div class="box">
-                <div class="white-box">
-                    <img src="../assets/img/involvement/contextualized.jpg"/>
-                    <br/>
-                    CONTEXTUALIZED MINISTRIES
-                </div>
+                <a href="http://crucentralcoast.com/">
+                    <div class="white-box">
+                        <img src="../assets/img/involvement/contextualized.jpg"/>
+                        <br/>
+                        CONTEXTUALIZED MINISTRIES
+                    </div>
+                </a>
             </div>
             <div class="box">
-                <div class="white-box">
-                    <img src="../assets/img/involvement/summer.jpg"/>
-                    <br/>
-                    SUMMER MISSIONS / STINT
-                </div>
+                <a href="<?php echo base_url() . "summer" ?>">
+                    <div class="white-box">
+                        <img src="../assets/img/involvement/summer.jpg"/>
+                        <br/>
+                        SUMMER MISSIONS / STINT
+                    </div>
+                </a>
             </div>
         </div>
         <div class="right col-md-4">
+            <div class="list-item">
+                <div class="list-icon">
+                    <i class="fa fa-calendar fa-3x"></i>
+                </div>
+                <div class="list-text">
+                    UPCOMING EVENTS
+                </div>
+            </div>
+            <br/>
+            
+            <?php foreach($events as $event) { ?>
             <?php
-            //echo count($events);
-            //print_r($events);
+                $timestamp = strtotime($event->Date);
+                $month = date("M", $timestamp);
+                $day = date("j", $timestamp);
             ?>
-            Test2
+            <div class="list-item-event">
+                <div class="date-container">
+                    <div class="event-date">
+                        <div class="month"><?php echo strtoupper($month); ?></div>
+                        <div class="day"><?php echo $day; ?></div>
+                    </div>
+                </div>
+                <div class="event-text">
+                    <div class="event-text-title">
+                        <?php
+                            $str = $event->Name . " (";
+                            $starttime = strtotime($event->StartTime);
+                            if(isset($event->EndTime)) {
+                                $endtime = strtotime($event->EndTime);
+                            }
+                            else {
+                                $endtime = NULL;
+                            }
+
+                            $str .= date("g", $starttime);
+                            if(date("i", $starttime) !== "00") {
+                                $str .= ":" . date("i", $starttime);
+                            }
+                            
+                            if($endtime !== NULL) {
+                                $str .= "&ndash;";
+                                $str .= date("g", $endtime);
+                                if(date("i", $endtime) !== "00") {
+                                    $str .= ":" . date("i", $endtime);
+                                }
+                                $str .= date("a", $endtime);
+                            }
+                            else {
+                                $str .= $str .= date("a", $starttime);
+                            }
+                            
+                            $str .= ")";
+                            
+                            echo $str;
+                            //Weekly Meeting Week 1 (8pm)&ndash;
+                            ?>
+                    </div>
+                    <div class="event-text-desc">
+                        <?php echo $event->Location; ?>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
         </div>
     </div>
 </div>
