74fdc2647afae411a5a6c0d5c88d5aa82c66f75e
Kyle Reis <fedorareis@gmail.com>
2015-03-03 15:32:50 -0800

    started updating staff page.

--- htdocs/application/views/staff.php
+++ htdocs/application/views/staff.php
@@ -7,32 +7,36 @@
         //Since we have 2 Mission Team Leaders, we center those up top
         echo '<div class="row" style="padding: 10px 0 30px 0;">';
         echo '<div class="col-md-3"></div>';
-       /* for($i = 0; $i < 2; $i++) {
-            echo '<div class="col-md-3" style="text-align: center;">';
-            echo '<div style="font-size: 20pt; font-weight: 300;">';
-            echo $staff[$i]->Name;
-            echo '</div>';
-            echo $staff[$i]->Role.'<br/>';
-            echo '<a href="mailto:'.$staff[$i]->Email.'" target="_blank">'.$staff[$i]->Email.'</a>';
-            echo '</div>';
-        }
-        echo '<div class="col-md-3"></div>';
-        echo '</div>';*/
+        
+        $group = '';
+        $groups = 0;
         
         //Loop through the rest of staff
-        for($i = 0; $i < count($staff); $i) {
-            echo '<div class="row" style="padding: 10px 0 30px 0;">';
+        for($i = 0; $i < count($staff); $i) 
+        {
+            if($group != $staff[$i]->group)
+            {
+                echo '<div class="' . $staff[$i]->group . '">';
+                $group = $staff[$i]->group;
+                $groups++;
+            }
+            echo '<div class="row">';
             
-            for($j = 0; $j < 3; $j++) {
-                echo '<div class="col-md-4" style="text-align: center;">';
-                echo '<div style="font-size: 20pt; font-weight: 300;">';
-                echo $staff[$i]->Name;
-                echo '</div>';
-                echo $staff[$i]->Role.'<br/>';
-                echo '<a href="mailto:'.$staff[$i]->Email.'" target="_blank">'.$staff[$i]->Email.'</a>';
+            for($j = 0; $j < 3; $j++) 
+            {
+                echo '<div class="card">';
+                echo '<img src="' . $staff[$i]->Picture . '" class="picture"></img>';
+                echo '<p class="name">' . $staff[$i]->Name . '</p>';
+                echo '<p class="role">' . $staff[$i]->Role . '</p><br/>';
+                echo '<a class="email" href="mailto:'.$staff[$i]->Email.'" target="_blank">'.$staff[$i]->Email.'</a>';
                 echo '</div>';
                 $i++;
             }
+            echo '</div>';
+        }
+        
+        while($groups--)
+        {
             echo '</div>';
         }
     ?>
