f003b5a52093a13abda367bb6c0d0fe7454448af
fedorareis <fedorareis@gmail.com>
2015-12-11 16:08:50 -0800

    updated copyright year

--- htdocs/application/views/footer/footer.php
+++ htdocs/application/views/footer/footer.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <p id="footer">&copy; 2015 Cru Central Coast. All Rights Reserved.<p>
+    <p id="footer">&copy; 2016 Cru Central Coast. All Rights Reserved.<p>
 </div>
 </body>
 </html>
